6b307732409f0eae0bc818bd2951503a05379727
Anirudh Singh Rathore <anirudh.rathore@bhnetwork.com>
2021-12-11 19:54:48 +0530

    Add Event Broadcaster to usecase

--- test/create-room.spec.js
+++ test/create-room.spec.js
@@ -1,48 +1,72 @@
 const expect = require("chai").expect;
 
-const { CreateRoom, InvalidIdError, RoomAlreadyExistsError } = require("../lib/usecase/create-room");
+const { CreateRoom, InvalidArgumentError, RoomAlreadyExistsError } = require("../lib/usecase/create-room");
 const RoomRepository = require("../lib/repositories/room.repository");
 const Room = require("../lib/entities/room");
+
+const MockEventBroadcaster = require("./mocks/mock-event-broadcaster");
+const StubConnection = require("./stubs/stub-connection");
 
 describe("Chat Room", function() {
 
   let roomRepository = null;
+  let eventBroadcaster = null;
 
   beforeEach(function() {
     roomRepository = new RoomRepository();
+    eventBroadcaster = new MockEventBroadcaster();
   });
 
   it("a new room can be created when valid id is given", function() {
     const useCase = createUseCase();
-    const roomId = "123456";
+    const input = createUseCaseInput();
 
-    const room = useCase.execute({ id: roomId });
+    const room = useCase.execute(input);
 
     expect(room).to.not.be.null;
-    expect(roomRepository.findById(roomId)).to.equal(room);
+    expect(roomRepository.findById(input.id)).to.equal(room);
   });
 
-  it("throws error when a new room is created with no id", function() {
+  it("a new room is created with a single user", function() {
     const useCase = createUseCase();
-
-    expect(() => useCase.execute()).to.throw(InvalidIdError);
+    const input = createUseCaseInput();
+    
+    useCase.execute(input);
+    
+    expect(eventBroadcaster.addedParticipantsToCorrectRoom(input));
   });
 
-  it("throws error when a new room is created with id as empty string", function() {
+  it("throws error when a new room is created with invalid data", function() {
     const useCase = createUseCase();
     
-    expect(() => useCase.execute({ id: "" })).to.throw(InvalidIdError);
+    expect(
+      () => useCase.execute(createUseCaseInput({id: null}))
+    ).to.throw(InvalidArgumentError);
+
+    expect(
+      () => useCase.execute(createUseCaseInput({userId: null}))
+    ).to.throw(InvalidArgumentError);
+
+    expect(
+      () => useCase.execute(createUseCaseInput({connection: null}))
+    ).to.throw(InvalidArgumentError);
   });
 
   it("throws error when a new room is created with a non-unique id", function() {
-    const id = "1234";
-    roomRepository.save(new Room(id)); 
     const useCase = createUseCase();
+    const input = createUseCaseInput();
+    roomRepository.save(new Room(input.id)); 
 
-    expect(() => useCase.execute({ id: id })).to.throw(RoomAlreadyExistsError);
+    expect(() => useCase.execute(input)).to.throw(RoomAlreadyExistsError);
   });
 
   function createUseCase() {
-    return new CreateRoom(roomRepository);
+    return new CreateRoom({roomRepository, eventBroadcaster});
+  }
+
+  function createUseCaseInput(overrides) {
+    return { id: "new-room-id", userId: "new-user-id", connection: new StubConnection(), ...overrides }
   }
 });
+
+
