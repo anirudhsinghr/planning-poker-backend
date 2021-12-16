4bf7f0d432d41a129110528a357c49a00cad90ed
Anirudh Singh Rathore <anirudh.rathore@bhnetwork.com>
2021-12-16 13:30:46 +0530

    Last memeber leaving causes the entire room to be purged

--- test/leave-room.spec.js
+++ test/leave-room.spec.js
@@ -7,6 +7,7 @@ const RoomNotFoundError = require("../lib/errors/room-not-found");
 const LeaveRoom = require("../lib/usecase/leave-room");
 const MockEventBroadcaster = require("./mocks/mock-event-broadcaster");
 const StubConnection = require("./stubs/stub-connection");
+const { createRoom, createAdmin,createVoterForRoom, createAdminForRoom } = require("./fixtures");
 
 describe("Leave Room", function() {
   let roomRepository = null;
@@ -20,16 +21,33 @@ describe("Leave Room", function() {
   });
 
   it("user gets removed from the room", function() {
-    const room = createRoomWithVoterIds(["1", "2"]);
+    const room = createRoom({ roomId: "new-room-id", roomRepository });
+    const v1 = createVoterForRoom({ voterId: "1", room, voterRepository });
+    const v2 = createVoterForRoom({ voterId: "2", room, voterRepository });
+    const input = { roomId: room.id, voterId: v1.id }
     const userCase = createUseCase();
-    const input = { roomId: room.id, voterId: "1" }
-    
+
     userCase.execute(input);
 
     expect(roomRepository.findById(room.id).voterCount()).to.equal(1);
     expect(voterRepository.findById(input.voterId)).to.be.undefined;
     expect(eventBroadcaster.voterLeftRoomWasCalledOnce()).to.be.true;
     expect(eventBroadcaster.voterLeftRoomWasCalledWithInput(input)).to.be.true;
+  });
+
+  it("admin is not removed from the storage", function() {
+    const room = createRoom({ roomId: "new-room-id", roomRepository });
+    const admin = createAdminForRoom({ adminId: "1", room, voterRepository });
+    const voter = createVoterForRoom({ voterId: "2", room, voterRepository });
+    const input = { roomId: room.id, voterId: admin.id }
+    const userCase = createUseCase();
+
+    userCase.execute(input);
+
+    expect(room.voterCount()).to.equal(1);
+    expect(room.voters).to.contain(voter);
+    expect(room.voters).to.not.contain(admin);
+    expect(voterRepository.findById(admin.id)).to.equal(admin);
   });
 
   it("given invalid room id will throw error", function() {
@@ -42,30 +60,32 @@ describe("Leave Room", function() {
     ).throws(RoomNotFoundError);
   });
 
-  it("Room is deleted with voters when last voter leaves", function() {
-    const room = createRoomWithVoterIds(["1"]);
+  it("Room is deleted with voters and admin when last voter leaves", function() {
+    const room = createRoom({ roomId: "new-room-id", roomRepository })
+    const voter = createVoterForRoom({ voterId: "new-voter-id", room, voterRepository });
+    const input = { roomId: room.id, voterId: voter.id }
     const userCase = createUseCase();
-    const input = { roomId: room.id, voterId: "1" }
+    
     userCase.execute(input);
     
     expect(roomRepository.findById(room.id)).to.be.undefined;
-    expect(voterRepository.findById("1")).to.be.undefined;
+    expect(voterRepository.findById(voter.id)).to.be.undefined;
   });
+  
+  it("Room is deleted with admin when last voter leaves", function() {
+    const room = createRoom({ roomId: "new-room-id", roomRepository })
+    const admin = createAdmin({ adminId: "new-admin-id", roomId: room.id, voterRepository });
+    const voter = createVoterForRoom({ voterId: "new-voter-id", room, voterRepository });
+    const input = { roomId: room.id, voterId: voter.id }
+    const userCase = createUseCase();
+    
+    userCase.execute(input);
 
-  function createRoomWithVoterIds(votersIds = ["1"]) {
-    const room = createVotersForRoom(new Room("new-room-id"), votersIds);
-    roomRepository.save(room);
-    return room;
-  }
-
-  function createVotersForRoom(room, voterIds) {
-    voterIds.forEach((id) => {
-      const voter = new Voter(id, room.id, new StubConnection());
-      room.addVoter(voter)
-      voterRepository.save(voter);
-    });
-    return room;
-  }
+    expect(room.isEmpty()).to.be.true;
+    expect(roomRepository.findById(room.id)).to.be.undefined;
+    expect(voterRepository.findById(admin.id)).to.be.undefined;
+    expect(voterRepository.findById(voter.id)).to.be.undefined;
+  });
 
   function createUseCase() {
     return new LeaveRoom({ roomRepository, voterRepository, eventBroadcaster });
