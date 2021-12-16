f88e16d0356cdbed2bf3ce8f01bb3527f4a90bdd
Anirudh Singh Rathore <anirudh.rathore@bhnetwork.com>
2021-12-16 12:26:41 +0530

    Allow admins to force join room instances

--- test/join-room.spec.js
+++ test/join-room.spec.js
@@ -1,14 +1,15 @@
-const Room = require("../lib/entities/room");
-const Voter = require("../lib/entities/voter");
-const Admin = require("../lib/entities/admin");
+const { expect } = require("chai");
+
 const JoinRoom = require("../lib/usecase/join-room");
 const RoomRepository = require("../lib/repositories/room.repository");
+const VoterRepository = require("../lib/repositories/voter.repository");
+
 const MockEventBroadcaster = require("./mocks/mock-event-broadcaster");
 const StubConnection = require("./stubs/stub-connection");
+
 const RoomNotFoundError = require("../lib/errors/room-not-found");
 
-const { expect } = require("chai");
-const VoterRepository = require("../lib/repositories/voter.repository");
+const { createRoom, createAdmin, createVoter } = require("./fixtures/index");
 
 describe("Join Room", function() {
 
@@ -23,9 +24,9 @@ describe("Join Room", function() {
   });
 
   it("Voter joining an existing room is broadcasted to all members in the room", function() {
-    const room = createRoom("123456");
+    const room = createRoom({ roomId: "new-room-id", roomRepository });
+    const input = createUseCaseInput({ roomId: room.id });
     const useCase = new createUsecase();
-    const input = createUseCaseInput({ roomId: "123456" });
     
     useCase.execute(input);
 
@@ -38,48 +39,33 @@ describe("Join Room", function() {
   it("Voter joining a non-existant room throws error", function() {
     const useCase = new createUsecase();
     const input = createUseCaseInput({ roomId: "non-existant" });
+    
     expect(
       () => useCase.execute(input)
     ).to.throw(RoomNotFoundError);
   });
 
   it("Admins can join the same room again", function() {
-    const room = createRoom("room-id");
-    const admin = createAdmin({ roomId: room.id, adminId: "admin-id" });
-    const useCase = new createUsecase();
+    const room = createRoom({ roomId: "new-room-id", roomRepository });
+    const admin = createAdmin({ roomId: room.id, adminId: "admin-id", voterRepository });
     const input = createUseCaseInput({ roomId: room.id, voterId: admin.id });
+    const useCase = new createUsecase();
     
-    useCase.execute(input)
+    useCase.execute(input);
+
     expect(room.admin()).to.equal(admin);
   });
 
   it("Voters can join the same room again", function() {
-    const room = createRoom("room-id");
-    const voter = createVoter({ roomId: room.id, voterId: "voter-id" });
+    const room = createRoom({ roomId: "new-room-id", roomRepository });
+    const voter = createVoter({ roomId: room.id, voterId: "voter-id", voterRepository });
     const useCase = new createUsecase();
     const input = createUseCaseInput({ roomId: room.id, voterId: voter.id });
     
-    useCase.execute(input)
+    useCase.execute(input);
+
     expect(room.voters).to.contain(voter);
   });
-
-  function createRoom(id) {
-    const room = new Room(id);
-    roomRepository.save(room);
-    return room;
-  }
-
-  function createAdmin({ roomId, adminId }) {
-    const admin = new Admin(adminId, roomId, new StubConnection());
-    voterRepository.save(admin);
-    return admin;
-  }
-
-  function createVoter({ roomId, voterId }) {
-    const voter = new Voter(voterId, roomId, new StubConnection());
-    voterRepository.save(voter);
-    return voter;
-  }
 
   function createUsecase() {
     return new JoinRoom({roomRepository, voterRepository, eventBroadcaster});
